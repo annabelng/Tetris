68af6648e40c1e6a2310327e1115a8b180ba0ee1
annabelng <45742119+annabelng@users.noreply.github.com>
2018-12-11 14:12:46 -0800

    Add files via upload

---
+++ lBlock.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class lBlock {
+	private int x;
+	private int y;
+	private int vy, vx;
+	private int w, h;
+	private JLabel img;
+
+	public lBlock(String filename) {
+		String src = new File("").getAbsolutePath() + "/src/";
+		ImageIcon rectangleIcon = new ImageIcon(src + filename);
+		// connect the file to the img
+		img = new JLabel(rectangleIcon);
+		w = 150;
+		h = 100;
+		x = 250;
+		y = 150;
+		img.setBounds(x, y, w, h);
+	}
+	
+	public int getX(){
+		return x; //getter for objects x field
+	}
+	public void setX(int newX) {
+		x = newX; // update object
+		// object object's image
+		img.setBounds(x, y, w, h);
+	}
+	public JLabel getImg(){
+		return img; //getter for object's img
+	}
+}
